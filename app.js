0cd7a0e57d4bbc4f654492fae7e416c8fbd91ac9
Md. Reyanus Salehin <salehin24.rs@gmail.com>
2020-12-12 20:08:26 +0600

    some changed in index

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('view engine','ejs');
 //adding static assets for html files
 app.use(express.static(__dirname + "/node_modules"));
 app.use(express.static(__dirname + "/css"));
+app.use(express.static(__dirname + "/images"));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
